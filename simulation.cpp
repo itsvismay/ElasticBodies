9ce433a014f71a7fd7080c3148804fc36b219f36
vismay <vismay@utexas.edu>
2016-05-11 21:35:20 -0500

    added beam and fixed rendering

--- simulation.cpp
+++ simulation.cpp
@@ -531,20 +531,44 @@ bool drawLoopTest(igl::viewer::Viewer& viewer){
 }
 
 bool drawLoop(igl::viewer::Viewer& viewer){
-	viewer.data.clear();
 	cout<<Sim.t<<endl;
 	Sim.render();
 
-	for(unsigned int i=0; i<Sim.mapV2TV.size(); i++){
-		V.row(i) = Sim.TV.row(Sim.mapV2TV[i]);
+	double refinement = 9;
+	double t = ((refinement - 1)+1) / 9.0;
+
+	VectorXd v = B.col(2).array() - B.col(2).minCoeff();
+	v /= v.col(0).maxCoeff();
+
+	vector<int> s;
+	for (unsigned i=0; i<v.size();++i){
+		if (v(i) < t){
+			s.push_back(i);
+		}
 	}
 
+	MatrixXd V_temp(s.size()*4,3);
+	MatrixXi F_temp(s.size()*4,3);
+
+	for (unsigned i=0; i<s.size();++i)
+	{
+		V_temp.row(i*4+0) = Sim.TV.row(TT(s[i],0));
+		V_temp.row(i*4+1) = Sim.TV.row(TT(s[i],1));
+		V_temp.row(i*4+2) = Sim.TV.row(TT(s[i],2));
+		V_temp.row(i*4+3) = Sim.TV.row(TT(s[i],3));
+		F_temp.row(i*4+0) << (i*4)+0, (i*4)+1, (i*4)+3;
+		F_temp.row(i*4+1) << (i*4)+0, (i*4)+2, (i*4)+1;
+		F_temp.row(i*4+2) << (i*4)+3, (i*4)+2, (i*4)+0;
+		F_temp.row(i*4+3) << (i*4)+1, (i*4)+2, (i*4)+3;
+	}
+	viewer.data.clear();
+	
 	viewer.data.add_edges(RowVector3d(0,0,0), RowVector3d(100,0,0), RowVector3d(1,1,1));
 	viewer.data.add_edges(RowVector3d(0,0,0), RowVector3d(0,100,0), RowVector3d(1,1,1));
 	//viewer.data.add_edges(RowVector3d(0,0,0), RowVector3d(0,0,100), RowVector3d(1,1,1));
-	
-	viewer.data.set_mesh(V, F);
-	viewer.data.set_face_based(false);
+	viewer.data.set_mesh(V_temp,F_temp);
+	viewer.data.set_face_based(true);
+	return false;
 	return false;
 }
 
@@ -552,10 +576,12 @@ void useFullObject(bool headless, float timestep, int iterations, char method){
 	vector<int> mapV2TV;
 	// Load a surface mesh
 	// igl::readOFF(TUTORIAL_SHARED_PATH "/3holes.off",V,F);
-	igl::readOBJ(TUTORIAL_SHARED_PATH "/wheel.obj", V, F);
+	igl::readOBJ(TUTORIAL_SHARED_PATH "/beam.obj", V, F);
 	V = V;
 	// Tetrahedralize the interior
-	igl::copyleft::tetgen::tetrahedralize(V,F,"-pq1.414Y", TV,TT,TF);
+	// igl::copyleft::tetgen::tetrahedralize(V,F,"-pq1.414Y", TV,TT,TF);
+	igl::copyleft::tetgen::tetrahedralize(V,F,"-pq2/0", TV,TT,TF);
+	
 	// // Compute barycenters
 	igl::barycenter(TV, TT,B);
 	// //constructs the map from V to TV
@@ -568,7 +594,14 @@ void useFullObject(bool headless, float timestep, int iterations, char method){
 		}
 	}
 	Sim.initializeSimulation(timestep, method, TT, TV, mapV2TV);
-	Sim.fixVertices(1);
+	//fix vertices
+	for(int i=0; i<Sim.vertices; i++){
+		if(Sim.TV.row(i)[0]<=-50){
+			cout<<Sim.TV.row(i)<<endl;
+			cout<<i<<endl;
+			Sim.fixVertices(i);
+		}
+	}
 	if(!headless){
 		igl::viewer::Viewer viewer;
 		viewer.callback_pre_draw = &drawLoop;
