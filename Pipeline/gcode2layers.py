174653ca3cbaae5ca04de44d0aa6abd88ad4c3d9
ZackMisso <zack441@mac.com>
2017-01-03 23:01:10 -0600

    Final Clean Up

--- Pipeline/gcode2layers.py
+++ Pipeline/gcode2layers.py
@@ -11,7 +11,7 @@ layerHeight = 0.4
 try:
   opts, args = getopt.getopt(sys.argv[1:], 'f', ["name="])
 except getopt.GetoptError:
-  print 'Error Bad Input'
+  #print 'Error Bad Input'
   sys.exit(-2)
 for opt, arg in opts:
   if opt == "--name":
@@ -45,71 +45,16 @@ class Extrusion:
     self.endW = self.calculateWidth(eX, eY, eF, sE, eE)
 
   def calculateWidth(self, x, y, f, e, pE):
-    # my hypothesis
+    #return 0.5
     deltaE = self.startE - self.endE
-    if deltaE < -1.0:
-      print 'ERROR ' + str(deltaE) + '\n'
+    #if deltaE < -1.0:
+    #  print 'ERROR ' + str(deltaE) + '\n'
     deltaD = np.sqrt((self.endX - self.startX)**2.0 + (self.endY - self.startY)**2)
     if deltaD == 0.0:
       return 0.0
     filArea = np.pi * (0.5 * filimentDiameter)**2.0
     heightFactor = layerHeight**2.0 * (np.pi / 4.0 - 1.0)
     return ( (deltaE / deltaD) * filArea - heightFactor ) / layerHeight
-    #return 0.5
-
-  def collidesWithY(self, y):
-    fourCorners = self.getFourCorners() # can improve performance by caching
-    maxY = -1000
-    minY = 1000
-    for i in range(len(fourCorners)):
-      if fourCorners[i][1] > maxY:
-        maxY = fourCorners[i][1]
-      if fourCorners[i][1] < minY:
-        minY = fourCorners[i][1]
-    return y >= minY and y <= maxY
-
-  def collidesWithPoint(self, y, x):
-    #fourCorners = self.getFourCorners() # can improve performance by caching
-    distVec = np.array([self.endX - self.startX, self.endY - self.startY])
-    pointVec = np.array([x - self.startX, y - self.startY])
-    moveVec = np.cross(distVec, np.array([0.0, 0.0, 1.0]))
-    # projDistVec = (pointVec dot norm(distVec)
-    # projMovVec = ((projDistVec - pointVec) dot norm(moveVec))
-    # to be implemented
-    return False
-
-  def getFourCorners(self):
-    fourCorners = []
-    halfStartWidth = self.startW / 2.0
-    halfEndWidth = self.endW / 2.0
-    distVec = np.array([self.endX - self.startX, self.endY - self.startY, 0.0])
-    moveVec = np.cross(distVec, np.array([0.0, 0.0, 1.0]))
-    fourCorners.append([self.startX + moveVec[0], self.startY + moveVec[1]])
-    fourCorners.append([self.startX - moveVec[0], self.startY - moveVec[1]])
-    fourCorners.append([self.endX - moveVec[0], self.endY - moveVec[1]])
-    fourCorners.append([self.endX + moveVec[0], self.endY + moveVec[1]])
-    return fourCorners
-
-class Row:
-  extrusionsInRow = []
-  rowData = []
-  rowY = 0.0
-  dim = 0.0
-  numRow = 0
-  layer = -1
-
-  def __init__(self, extrusions, y, d, numR, startX, lay):
-    self.rowY = y
-    self.dim = d
-    self.numRow = numR
-    self.layer = lay
-    getExtrusionsInRow()
-
-  def getExtrusionsInRow(self, extrusions, startX):
-    for ex in extrusions:
-      if (ex.layer == self.layer):
-        if (ex.collidesWithY(self.rowY) == True):
-          rowData.append(ex)
 
 def loadData(fileName):
   fr = open(fileName)
@@ -124,7 +69,7 @@ def loadData(fileName):
   return Data, count
 
 def cleanData(rawData, numLines):
-  
+
   newData = []
   extrusions = []
   count = 0;
@@ -159,15 +104,10 @@ def cleanData(rawData, numLines):
       if (not (tokenX == prevX and tokenY == prevY and tokenE == prevE) ):
         if (tokenX > -1.0 and tokenY > -1.0 and tokenE > -1.0 and tokenZ == -1.0):
           # create extrusion
-          #print str(tokenE), ' :: ', str(prevE)
           newExtrusion = Extrusion(tokenX, tokenY, tokenE, prevF, prevX, prevY, prevE, prevF, layer)
-          #print str(tokenE), ' :: ', str(prevE)
-          extrusions.append([layer, newExtrusion])
           newData.append([layer, tokenX, tokenY, newExtrusion.startW]) # replace with the extrusion width soon
         elif (tokenX > -1.0 and tokenY > -1.0):
           newData.append([layer, tokenX, tokenY, 0.0])
-      #if (tokenF > -1.0):
-      #  prevF = tokenF
       if (tokenE > -1.0):
         prevE = tokenE
       if (tokenX > -1.0):
@@ -177,45 +117,14 @@ def cleanData(rawData, numLines):
       if (tokenZ > -1.0):
         prevX = -1.0
         prevY = -1.0
-        #prevE = -1.0
         layer += 1
 
   return newData, extrusions, count, layer
 
-def getVoxelDimensions(extrusions):
-  minX = 10000
-  minY = 10000
-  maxX = -10000
-  maxY = -10000
-  minW = 10000
-
-  for ex in extrusions:
-    if (minX > ex.startX):
-      minX = ex.startX
-    if (minX > ex.endX):
-      minX = ex.endX
-    if (minY > ex.startY):
-      minY = ex.startY
-    if (minY > ex.endY):
-      minY = ex.endY
-    if (maxX < ex.startX):
-      maxX = ex.startX
-    if (maxX < ex.endX):
-      maxX = ex.endX
-    if (maxY < ex.startY):
-      maxY = ex.startY
-    if (maxY < ex.endY):
-      maxY = ex.endY
-    if (minW > ex.startW):
-      minW = ex.startW
-
-  dim = minW / 3.0
-  return dim, minX, maxX, minY, maxY
-
-print vName
-Data_old, numLines_old = loadData(vName); print numLines_old
+#print vName
+Data_old, numLines_old = loadData(vName); #print numLines_old
 Data, extrusions, numLines, numLayers = cleanData(Data_old, numLines_old)
-print numLines, numLayers
+#print numLines, numLayers
 
 #-------------------------------------------------
 # Code for writing the layer files into scad
@@ -227,15 +136,54 @@ DataNP = np.array(Data)
 runLayers = range(numLayers+1)
 for layer in runLayers:
   layerData = DataNP[indices == layer]
-  filename = "CCgcode_layer" + str(layer) + ".scad"
+  filename = vName[:-6]+"_layer_" + str(layer) + ".scad"
   file_write = open(filename, "w")
+  # write needed modules
+  file_write.write("module drawBasicShape(x1, y1, x2, y2, width)\n")
+  file_write.write("{\n")
+  file_write.write("\tlength = sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));\n")
+  file_write.write("\tif (width!=0.0) {\n")
+  file_write.write("\t\tsquare(size = [width, length], center = false);\n")
+  file_write.write("\t\ttranslate([width/2.0, 0, 0]) circle(d=width);\n")
+  file_write.write("\t\ttranslate([width/2.0, length, 0]) circle(d=width);\n")
+  file_write.write("\t}\n")
+  file_write.write("}\n")
+  file_write.write("\n")
+  file_write.write("module translateAndRotate(x1, y1, x2, y2, width)\n")
+  file_write.write("{\n")
+  file_write.write("\tif (width != 0.0) {\n")
+  file_write.write("\t\tangle = atan((y2-y1)/(x2-x1)) +270;\n")
+  file_write.write("\t\tif (x2>=x1) {\n")
+  if layer == 0:
+    file_write.write("\t\t\ttranslate([x1, y1, 0.0]) rotate([0.0, 0.0, angle]) translate([-width/2.0, 0.0, 0.0]) drawBasicShape(x1, y1, x2, y2, width);\n")
+  else:
+    file_write.write("\t\t\ttranslate([x1, y1, 0.0]) rotate([0.0, 0.0, angle]) translate([-width/2.0, 0.0, 0.0]) scale([1.0, 1.0, 1.0]) drawBasicShape(x1, y1, x2, y2, width);\n")
+  file_write.write("\t\t}\n")
+  file_write.write("\t\telse {\n")
+  if layer == 0:
+    file_write.write("\t\t\ttranslate([x1, y1, 0.0]) rotate([0.0, 0.0, angle+180]) translate([-width/2.0, 0.0, 0.0]) drawBasicShape(x1, y1, x2, y2, width);")
+  else:
+    file_write.write("\t\t\ttranslate([x1, y1, 0.0]) rotate([0.0, 0.0, angle+180]) translate([-width/2.0, 0.0, 0.0]) scale([1.0, 1.0, 1.0]) drawBasicShape(x1, y1, x2, y2, width);")
+  file_write.write("\t\t}\n")
+  file_write.write("\t}\n")
+  file_write.write("}\n")
+  file_write.write("\n")
+  file_write.write("$fn=50;\n")
+  file_write.write("\n")
+
+  # write actual data
   file_write.write('points' + str(layer) + ' = [\n')
   for row in layerData:
-    file_write.write("[%7.3f, %8.3f, %2.6f],\n" % (row[1], row[2], row[3]))
+    file_write.write("[%7.8f, %8.8f, %2.8f],\n" % (row[1], row[2], row[3]))
   file_write.write("];\n")
+  file_write.write("\n")
   z += layerThk
-  file_write.write("translate([0.0, 0.0, %6.3f]) {\n" % (z))
-  file_write.write("\tlinear_extrude(height = %6.3f, center = false, convexity = 10, twist = 0) {\n" % (layerThk))
+  #file_write.write("translate([0.0, 0.0, %6.3f]) {\n" % (z - (layerThk * .005)))
+  file_write.write("translate([0.0, 0.0, %6.8f]) {\n" % z)
+  if layer == 0:
+    file_write.write("\tlinear_extrude(height = %6.8f, center = false, convexity = 10, twist = 0) {\n" % (layerThk))
+  else:
+    file_write.write("\tlinear_extrude(height = %6.8f, center = false, convexity = 10, twist = 0) {\n" % (layerThk * 1.01))
   file_write.write("\t\tunion() {\n")
   file_write.write("\t\t\tfor( i=[0:len(points" + str(layer) + ")-2] ){ \n")
   file_write.write("\t\t\t\tx1 = points" + str(layer) + "[i][0];\n")
@@ -250,8 +198,12 @@ for layer in runLayers:
   file_write.write("}\n")
   file_write.close()
 
-filename = "CCgcode_for_loop.scad"
-file_write = open(filename, "w")
-for layer in runLayers:
-  file_write.write("include <CCgcode_layer%d.scad>;\n" % (layer))
-file_write.close()
+#filename = "CCgcode_for_loop.scad"
+#file_write = open(filename, "w")
+#for layer in runLayers:
+#  file_write.write("include <"+vName[:-6]+"_layer_%d.scad>;\n" % (layer))
+#file_write.close()
+
+print numLayers
+
+#sys.exit(numLayers)
