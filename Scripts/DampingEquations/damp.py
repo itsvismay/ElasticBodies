6c115a47f126ff891a4b958dff51129739541a97
Vismay <vismay@utexas.edu>
2017-02-27 20:32:26 -0600

    messing with values for analytical solution

--- Scripts/DampingEquations/damp.py
+++ Scripts/DampingEquations/damp.py
@@ -3,26 +3,26 @@ import math, sys
 import matplotlib.pyplot as plt
 
 
-print("Mass: " + sys.argv[1] +" Gravity:" +sys.argv[2])
-m = 1
+print("Mass: " + sys.argv[1] +" Length:" +sys.argv[2])
+m = float(sys.argv[1])
 g = 10
 #mg/k = L
 #|----l---|--L--|
 #initial diisplacment takes length back to original spring length l
 # u(0) = -L = -mg/k
 
-k = 4
+k = 8e4
 rayleigh1 = 0
-rayleigh2 = 0
-c = 0 #rayleigh1*m + rayleigh2*k
+rayleigh2 = 0.0001
+c = rayleigh1*m + rayleigh2*k
 
 #this is u
 init_pos = -1*m*g/k
 init_vel = 0
 check = c*c - 4*m*k
 #x = exp( -c*t/(2m)) *[c1 cos(wt) + c2 sin(wt)], w = sqrt(4mk - c^2)/(2m)
-t = 1e-4
-sim_seconds = 5
+t = 1e-5
+sim_seconds = 0.2
 
 time = []
 data = []
@@ -51,7 +51,7 @@ for i in range (0, int(sim_seconds/t)):
         c2 = (init_vel  + c*c1/(2*m))/w
         x = math.exp(-1*c*t*i/m/2)*(c1*math.cos(w*t*i) + c2*math.sin(w*t*i))
     time.append(t*i)
-    data.append(x)
+    data.append(x- float(sys.argv[2]))
 
 plt.plot(time, data, "-", label="curve")
 plt.show()
