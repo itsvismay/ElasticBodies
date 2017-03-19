1deb4b94469de3e61ed5f52feb938f6533cdabb9
Vismay <vismay@utexas.edu>
2017-03-19 00:27:39 -0500

    work on the constrained motion stuff later

--- Scripts/BeamTestOpenCV/plotdata.py
+++ Scripts/BeamTestOpenCV/plotdata.py
@@ -2,12 +2,13 @@ import matplotlib.pyplot as plt
 t = []
 x = []
 f = open('redbeamdata.txt')
-f1 = open('../../TestsResults/Boba/tensile_beam/rawTensile@pRa7.5position.txt')
+f1 = open('../../TestsResults/Boba/tensile_beam/rawTensile@pRa5position.txt')
 for line in f:
 	
 	step = line.strip("\n").split(",")
 	#t.append(int(step[0]))
-	t.append(((int(step[0]) - 982)/2051.0)-0.08678)#*86/2051)	
+	#t.append(((int(step[0]) - 982)/2051.0)-0.08678)#*86/2051)	
+	t.append(((int(step[0]) - 982)/2051.0)-0.01)
 	x.append(float(step[1]))
 
 t1 = []
@@ -17,7 +18,7 @@ for line in f1:
 	t1.append(int(step[0])*1e-5)
 	x1.append(float(step[1]))
 
-plt.plot(t[210:], x[210:], "b", label='RealBeamData')
+plt.plot(t[0:], x[0:], "b", label='RealBeamData')
 print(t[211])
 plt.plot(t1, x1, "g-", label="TestData")
 plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=4, mode="expand", borderaxespad=0.)
